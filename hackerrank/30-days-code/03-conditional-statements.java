cf468507b98b2e7a005665972d10c73672974455
thomasmartinez114 <tmartinez114@gmail.com>
2020-04-09 23:27:14 -0400

    added boiler plate code

--- hackerrank/30-days-code/03-conditional-statements.java
+++ hackerrank/30-days-code/03-conditional-statements.java
@@ -41,3 +41,23 @@
 
 // Sample Case 1:
 // and is even, so it isn't weird. Thus, we print Not Weird.
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+    }
+}
