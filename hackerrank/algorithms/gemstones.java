5bfaf36c04b6a6934a19c9a35dbb97a9c868a93b
Thomas Martinez <thomas.martinez@igt.com>
2020-04-21 21:09:53 -0400

    added Gemstone **not completed**

---
+++ hackerrank/algorithms/gemstones.java
@@ -0,0 +1,50 @@
+// https://www.hackerrank.com/challenges/gem-stones/problem?h_r=internal-search&isFullScreen=false
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the gemstones function below.
+    static int gemstones(String[] arr) {
+        for (int rock = 0; rock < arr.length; rock++) {
+            // System.out.println(arr[i]);
+            for (char mineral = 0; mineral < arr[rock].length(); mineral++){
+                // if (arr[rock][m])
+                System.out.println("Minerals in rock " + arr[rock].charAt(mineral));
+            }
+            System.out.println(arr[rock]);
+        }
+        return 5;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            String arrItem = scanner.nextLine();
+            arr[i] = arrItem;
+        }
+
+        int result = gemstones(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
