7ed5b42bfea1639ffa696348e1bb6d1d53f0d3cf
Thomas Martinez <tmartinez114@gmail.com>
2020-05-02 02:42:18 -0400

    completed right2

--- CodingBat/Strings/01/right2.java
+++ CodingBat/Strings/01/right2.java
@@ -8,6 +8,10 @@
 
 public String right2(String str) {
     String lastTwo = str.substring(str.length() - 2);
-    return lastTwo;
+    // return lastTwo;
+    
+    String remaining = str.substring(0, str.length() - 2);
+    // return remaining;
+    
+    return lastTwo + remaining;
   }
-  
