e1ed94e4498ecb61796616a6eace5e52a9d10e4f
Thomas Martinez <tmartinez114@gmail.com>
2020-05-01 23:23:46 -0400

    notes on Stdin & Stdout

---
+++ hackerrank/scanner-in-out/01-stdin-stdout.java
@@ -0,0 +1,17 @@
+// Most HackerRank challenges require you to read input from stdin (standard input) and write output to stdout (standard output).
+
+// One popular way to read input from stdin is by using the Scanner class and specifying the Input Stream as System.in. For example:
+// Scanner scanner = new Scanner(System.in);
+// String myString = scanner.next();
+// int myInt = scanner.nextInt();
+// scanner.close();
+
+// System.out.println("myString is: " + myString);
+// System.out.println("myInt is: " + myInt);
+
+// The code above creates a Scanner object named scanner and uses it to read a String and an int. It then closes the Scanner object because there is no more input to read, and prints to stdout using System.out.println(String). So, if our input is:
+// Hi 5
+
+// Our code will print:
+// myString is: Hi
+// myInt is 5
