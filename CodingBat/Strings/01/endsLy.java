a6f919975ac02f4b2531db979823af56329d592c
Thomas Martinez <tmartinez114@gmail.com>
2020-05-03 00:12:27 -0400

    completed endsLy

---
+++ CodingBat/Strings/01/endsLy.java
@@ -0,0 +1,16 @@
+
+// Given a string, return true if it ends in "ly".
+
+
+// endsLy("oddly") → true
+// endsLy("y") → false
+// endsLy("oddy") → false
+
+public boolean endsLy(String str) {
+    if (str.length() < 2) {
+      return false;
+    }
+    else {
+      return str.substring(str.length() - 2).equals("ly");
+    }
+   }
