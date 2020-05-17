37456129dbb0914edc33f24430cfd8125cf0be45
Thomas Martinez <tmartinez114@gmail.com>
2020-05-16 23:58:48 -0400

    updated detail

--- CodingBat/Strings/01/nTwice.java
+++ CodingBat/Strings/01/nTwice.java
@@ -1,7 +1,6 @@
 
 // Given a string and an int n, return a string made of the first and last n chars from the string. The string length will be at least n.
 
-
 // nTwice("Hello", 2) → "Helo"
 // nTwice("Chocolate", 3) → "Choate"
 // nTwice("Chocolate", 1) → "Ce"
