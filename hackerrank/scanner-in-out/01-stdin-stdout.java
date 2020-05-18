e935f67407f05d5dadb0905af37fc7788f903e8f
Martinez <thomas.martinez@igt.com>
2020-05-18 00:06:24 -0400

    update

--- hackerrank/scanner-in-out/01-stdin-stdout.java
+++ hackerrank/scanner-in-out/01-stdin-stdout.java
@@ -17,7 +17,7 @@
 // Scanner scanner = new Scanner(System.in);
 // String myString = scanner.next();
 // int myInt = scanner.nextInt();
-// scanner.close();
+// scanner.close();;
 
 // System.out.println("myString is: " + myString);
 // System.out.println("myInt is: " + myInt);
