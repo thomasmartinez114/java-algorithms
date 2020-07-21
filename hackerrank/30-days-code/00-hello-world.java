519500ec3b13de5c0b2efe8248c73b1de209daa5
Thomas Martinez <tmartinez114@gmail.com>
2020-07-20 23:47:09 -0400

    00

--- hackerrank/30-days-code/00-hello-world.java
+++ hackerrank/30-days-code/00-hello-world.java
@@ -33,6 +33,34 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+// public class Solution {
+//     public static void main(String[] args) {
+//         // Create a Scanner object to read input from stdin.
+//         Scanner scan = new Scanner(System.in);
+
+//         // Read a full line of input from stdin and save it to our variable,
+//         // inputString.
+//         String inputString = scan.nextLine();
+
+//         // Close the scanner object, because we've finished reading
+//         // all of the input from stdin needed for this challenge.
+//         scan.close();
+
+//         // Print a string literal saying "Hello, World." to stdout.
+//         System.out.println("Hello, World.");
+//         System.out.println(inputString);
+
+//         // TODO: Write a line of code here that prints the contents of inputString to
+//         // stdout.
+//     }
+// }
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
 public class Solution {
     public static void main(String[] args) {
         // Create a Scanner object to read input from stdin.
@@ -48,9 +76,9 @@ public class Solution {
 
         // Print a string literal saying "Hello, World." to stdout.
         System.out.println("Hello, World.");
-        System.out.println(inputString);
 
         // TODO: Write a line of code here that prints the contents of inputString to
         // stdout.
+        System.out.println(inputString);
     }
 }
