f9a143d9e705645a0d47d03e150f46d6cc075a60
Thomas Martinez <tmartinez114@gmail.com>
2020-07-25 20:07:22 -0400

    dateFashion - SUCCESS

--- CodingBat/Logic/01/dateFashion.java
+++ CodingBat/Logic/01/dateFashion.java
@@ -7,10 +7,10 @@
 // dateFashion(5, 5) → 1
 
 public int dateFashion(int you, int date) {
-  if (you >= 8 || date >= 8)
-    return 2;
   if (you <= 2 || date <= 2)
     return 0;
+  else if (you >= 8 || date >= 8)
+    return 2;
   else
     return 1;
 }
