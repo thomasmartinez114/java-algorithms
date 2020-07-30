c9845318ab345af56154a51f73466f0c4543c8b9
Thomas Martinez <tmartinez114@gmail.com>
2020-07-29 23:42:20 -0400

    completed rotateLeft3

---
+++ CodingBat/Arrays/01/rotateLeft3.java
@@ -0,0 +1,17 @@
+
+// Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+
+
+// rotateLeft3([1, 2, 3]) → [2, 3, 1]
+// rotateLeft3([5, 11, 9]) → [11, 9, 5]
+// rotateLeft3([7, 0, 0]) → [0, 0, 7]
+
+public int[] rotateLeft3(int[] nums) {
+  int[] newArr = new int[3];
+  
+  newArr[0] = nums[1];
+  newArr[1] = nums[2];
+  newArr[2] = nums[0];
+  
+  return newArr;
+}
