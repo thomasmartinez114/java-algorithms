330a2b7f9fa0a2f5e07e02039de1ed8edd972279
Thomas Martinez <tmartinez114@gmail.com>
2020-08-11 00:07:09 -0400

    completed mapShare

---
+++ CodingBat/Map/01/mapShare.java
@@ -0,0 +1,18 @@
+
+// Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that same value. In all cases remove the key "c", leaving the rest of the map unchanged.
+
+
+// mapShare({"a": "aaa", "b": "bbb", "c": "ccc"}) → {"a": "aaa", "b": "aaa"}
+// mapShare({"b": "xyz", "c": "ccc"}) → {"b": "xyz"}
+// mapShare({"a": "aaa", "c": "meh", "d": "hi"}) → {"a": "aaa", "b": "aaa", "d": "hi"}
+
+public Map<String, String> mapShare(Map<String, String> map) {
+  if (map.containsKey("a")) {
+    String temp = map.get("a");
+    map.put("b", temp);
+    map.remove("c");
+  } else {
+    map.remove("c");
+  }
+  return map;
+}
