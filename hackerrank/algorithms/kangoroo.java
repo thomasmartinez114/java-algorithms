367f2de26b9be22e0ce7c4c3627edd90029ec57b
Thomas Martinez <tmartinez114@gmail.com>
2020-08-22 20:35:55 -0400

    pseduo code added

--- hackerrank/algorithms/kangoroo.java
+++ hackerrank/algorithms/kangoroo.java
@@ -8,11 +8,19 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
+/*
+ * Problem:
+ * Kangaroo 1 starts at x1 and jumps v1
+ * Kangaroo 2 starts at x2 and jumps v2
+ * Need to figure out how to get the kangaroos at the same location
+ * If possible, return YES, if not NO
+ * 
+*/
+
 public class Solution {
 
     // Complete the kangaroo function below.
     static String kangaroo(int x1, int v1, int x2, int v2) {
-        int jumpes = 0;
 
     }
 
